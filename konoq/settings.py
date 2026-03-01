dab0c88e86ff84dbfc293464cc405757a63c34a1
futurespice <azatss442@gmail.com>
2026-03-01 15:33:34 +0600

    fix статики

--- konoq/settings.py
+++ konoq/settings.py
@@ -119,7 +119,7 @@ USE_I18N      = True
 USE_TZ        = True
 
 # ── Static files ──────────────────────────────────────────────────────────────
-STATIC_URL  = "/static/"
+STATIC_URL  = "/django-static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
